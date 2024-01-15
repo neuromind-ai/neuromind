40671ba0d211a00d51e5bcfd9f1cb969611fb48c
Saurav Bharti <sauravbharti38@gmail.com>
2024-01-15 16:08:13 +0530

    second commit

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -19,10 +19,10 @@ export default function Footer() {
         <div className="link">
           <ul className="hyper_link">
             <li><a href="http://" target="_blank" rel="noopener noreferrer"><FaLinkedin />Linked In</a></li>
-            <li><a href="http://" target="_blank" rel="noopener noreferrer"><MdOutlineAlternateEmail />Email</a></li>
-            <li><a href="http://" target="_blank" rel="noopener noreferrer"><FaInstagram />Instagram</a></li>
+            <li><a href="http://neuromind.network@gmail.com" target="_blank" rel="noopener noreferrer"><MdOutlineAlternateEmail />Email</a></li>
+            <li><a href="http://instagram.com/neuromind_ai/" target="_blank" rel="noopener noreferrer"><FaInstagram />Instagram</a></li>
             <li><a href="http://" target="_blank" rel="noopener noreferrer"><FiMessageCircle />Message</a></li>
-            <li><a href="http://" target="_blank" rel="noopener noreferrer"><FaTwitter />Twitter</a></li>
+            <li><a href="http://twitter.com/neuromind_ai" target="_blank" rel="noopener noreferrer"><FaTwitter />Twitter</a></li>
             <li><a href="http://" target="_blank" rel="noopener noreferrer"><FaFacebook />Facebook</a></li>
           </ul>
         </div>
